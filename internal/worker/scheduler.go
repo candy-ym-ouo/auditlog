b03ecc02c4f21be7bac2385157ecf33bce5bba93
xiaochangfeng <xcfouo@163.com>
2026-08-23 09:55:55 +0800

    feat(auditlog): update asynchronous archive lifecycle

---
+++ internal/worker/scheduler.go
@@ -0,0 +1,24 @@
+package worker
+
+import (
+	"auditlog/internal/service"
+	"context"
+	"time"
+)
+
+func Start(ctx context.Context, s *service.ArchiveService, interval time.Duration) func() {
+	stop := make(chan struct{})
+	go func() {
+		t := time.NewTicker(interval)
+		defer t.Stop()
+		for {
+			select {
+			case <-t.C:
+				s.Trigger(ctx)
+			case <-stop:
+				return
+			}
+		}
+	}()
+	return func() { close(stop) }
+}
